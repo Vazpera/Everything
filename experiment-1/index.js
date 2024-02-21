bf7ae558ed03159a3314981835ab7083df998092
Vazpera <110002885+Vazpera@users.noreply.github.com>
2024-02-21 02:00:06 +0000

    Added Markdown Tab

--- experiment-1/index.js
+++ experiment-1/index.js
@@ -1,11 +1,16 @@
 console.log("Hello, world!");
+
 const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
+  	input: process.stdin,
+  	output: process.stdout
 });
 
 readline.question('Who are you?', name => {
-  console.log(`Hey there ${name}!`);
-  readline.close();
+  	console.log(`Hey there ${name}!`);
+	readline.close();
 });
 
+readline.question("How old are you?", age => {
+	console.log(`${18-age} untill you're 18!`);
+	readline.close();
+});
---
+++ markdown/test.md
@@ -0,0 +1,11 @@
+# This is a test Markdown document
+- Bullet Points
+## Secondary headers
+| Table? | Table. |
+|--------|--------|
+| 1      | 2      |
+
+```js
+console.log("Test");
+```
+`what?`
